f25af5246f0227437b374e7027cba4d0fa8efaa0
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-17 23:54:04 +0900

    Implement hooks movie search app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,103 @@
 import * as React from "react";
-import {hot} from "react-hot-loader";
+import {useEffect, useReducer} from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, World!</h1>
-            </div>
-        );
+const API_KEY = ''; // CHANGE ME TO YOUR API KEY
+const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&apikey=${API_KEY}`;
+
+const initialState = {
+    loading: true,
+    movies: [],
+    errorMessage: null
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SEARCH_MOVIES_REQUEST":
+            return {
+                ...state,
+                loading: true,
+                errorMessage: null
+            };
+        case "SEARCH_MOVIES_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                movies: action.payload
+            };
+        case "SEARCH_MOVIES_FAILURE":
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.error
+            };
+        default:
+            return state;
     }
-}
+};
 
-export default hot(module)(App);
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+
+        fetch(MOVIE_API_URL)
+            .then(response => response.json())
+            .then(jsonResponse => {
+
+                dispatch({
+                    type: "SEARCH_MOVIES_SUCCESS",
+                    payload: jsonResponse.Search
+                });
+            });
+    }, []);
+
+    const search = searchValue => {
+        dispatch({
+            type: "SEARCH_MOVIES_REQUEST"
+        });
+
+        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${API_KEY}`)
+            .then(response => response.json())
+            .then(jsonResponse => {
+                if (jsonResponse.Response === "True") {
+                    dispatch({
+                        type: "SEARCH_MOVIES_SUCCESS",
+                        payload: jsonResponse.Search
+                    });
+                } else {
+                    dispatch({
+                        type: "SEARCH_MOVIES_FAILURE",
+                        error: jsonResponse.Error
+                    });
+                }
+            });
+    };
+
+    const {movies, errorMessage, loading} = state;
+
+    return (
+        <div className="App">
+            <Header text="HOOKED"/>
+            <Search search={search}/>
+            <p className="App-intro">Sharing a few of our favourite movies</p>
+            <div className="movies">
+                {loading && !errorMessage ? (
+                    <span>loading... </span>
+                ) : errorMessage ? (
+                    <div className="errorMessage">{errorMessage}</div>
+                ) : (
+                    movies.map((movie, index) => (
+                        <Movie key={`${index}-${movie.Title}`} movie={movie}/>
+                    ))
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default App;
