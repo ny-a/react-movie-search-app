b615eeba4981c634040070c69a89d1f82b089d88
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-17 19:33:05 +0900

    Get rid of esModuleInterop

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
+import * as React from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div className="App">
